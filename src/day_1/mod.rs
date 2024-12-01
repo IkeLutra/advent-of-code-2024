2cdf10c4213272d597ed178781f57073fd78cb37
Will Otterburn <willotter@gmail.com>
2024-12-01 23:07:02 +0000

    Day 1 Puzzle 2

--- src/day_1/mod.rs
+++ src/day_1/mod.rs
@@ -1,30 +1,48 @@
-// Puzzle 1
-pub fn calculate_distance(data: String) -> isize {
-    let ints: Vec<Vec<isize>> = data
+fn convert_to_lists(data: String) -> (Vec<usize>, Vec<usize>) {
+    let ints: Vec<Vec<usize>> = data
         .split('\n')
         .map(|d| {
             d.split_whitespace()
-                .map(|s| str::parse::<isize>(s).unwrap())
+                .map(|s| str::parse::<usize>(s).unwrap())
                 .collect()
         })
         .collect();
-    let mut first: Vec<isize> = ints.iter().map(|d| d[0]).collect();
-    let mut second: Vec<isize> = ints.iter().map(|d| d[1]).collect();
+    let mut first: Vec<usize> = ints.iter().map(|d| d[0]).collect();
+    let mut second: Vec<usize> = ints.iter().map(|d| d[1]).collect();
     first.sort();
     second.sort();
-
+    (first, second)
+}
+// Puzzle 1
+pub fn calculate_distance(data: String) -> usize {
+    let (first, second) = convert_to_lists(data);
     first
         .iter()
         .zip(second.iter())
         .map(|(a, b)| {
-            return (b - a).abs();
+            return a.abs_diff(*b);
+        })
+        .sum()
+}
+
+pub fn calculate_simularity_score(data: String) -> usize {
+    let (left, right) = convert_to_lists(data);
+    left.into_iter()
+        .map(|d| {
+            d * (right
+                .iter()
+                .filter(|x| **x == d)
+                .collect::<Vec<&usize>>()
+                .len())
         })
         .sum()
 }
 
 #[cfg(test)]
 mod test {
-    use crate::day_1::calculate_distance;
+    use std::fs;
+
+    use crate::day_1::{calculate_distance, calculate_simularity_score};
 
     #[test]
     fn test_calculate_distance() {
@@ -35,5 +53,22 @@ mod test {
 3   9
 3   3";
         assert_eq!(calculate_distance(input.to_string()), 11);
+    }
+
+    #[test]
+    fn test_calculate_distance_real() {
+        let input = fs::read_to_string("./src/day_1/input.txt").expect("must be able to read file");
+        assert_eq!(calculate_distance(input), 1765812);
+    }
+
+    #[test]
+    fn test_simularity_score() {
+        let input = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+        assert_eq!(calculate_simularity_score(input.to_string()), 31);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,12 @@ mod day_1;
 use std::fs;
 
 use clap::{Parser, ValueEnum};
-use day_1::calculate_distance;
+use day_1::{calculate_distance, calculate_simularity_score};
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
 enum Puzzle {
     Day1Puzzle1,
+    Day1Puzzle2,
 }
 
 #[derive(Parser)]
@@ -25,6 +26,10 @@ fn main() {
         Puzzle::Day1Puzzle1 => {
             let result = calculate_distance(contents);
             println!("Distance is {}", result)
+        }
+        Puzzle::Day1Puzzle2 => {
+            let result = calculate_simularity_score(contents);
+            println!("Simularity Score is {}", result)
         }
     }
 }
